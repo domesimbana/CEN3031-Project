fb9764f142a57f56aeadf228b937ba2e7dea73c4
alexanderaziz <alexanderaziz04@hotmail.com>
2024-03-28 18:32:04 -0400

    Got react router to work

---
+++ frontend/client/src/screens/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Screen</h1>
+      {/* Add content for home screen */}
+    </div>
+  );
+};
+
+export default Home;
