fb958ada77c3451766459bf670d1501d92be470a
Tue.Tran <116848505+tue-tran-m@users.noreply.github.com>
2024-04-10 02:26:09 -0400

    Update App.js

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,24 +1,42 @@
-// src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './screens/Home';
-import About from './screens/About';
-import Contact from './screens/Contact';
-import Header from './components/Header';
+import OpenInterface from './components/open-window'
+import Work from './components/work-interface'
+import { jwtDecode } from "jwt-decode";
 
-const App = () => {
+import { useEffect, useState } from 'react';
+
+
+function App() {
+
+  const [user, setUser] = useState({});
+  const [display, setDisplay] = useState(false);
+
+  function handleCallbackResponse(res) {
+    console.log("Enconded JWT ID token: " + res.credential);
+    var userObject = jwtDecode(res.credential);
+    console.log(userObject);
+    setDisplay(true);
+    setUser(userObject);
+  }
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: "825357959721-817g515gj4tu01vmvgg5b234udnlsuid.apps.googleusercontent.com",
+      callback: handleCallbackResponse
+    });
+
+  }, []);
+
   return (
-    <Router>
-      <div>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
-    </Router>
+    <>
+      {!display &&
+        <OpenInterface handleCallback={handleCallbackResponse} />
+      }
+      {user && display &&
+        <Work userAcc={user} display={setDisplay} userSO={setUser} id="signInDiv" />
+      }
+    </>
   );
-};
+}
 
 export default App;
