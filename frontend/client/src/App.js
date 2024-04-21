e0dda7a940aea091f849cca5ec9648fdedb2cd3e
Tue.Tran <116848505+tue-tran-m@users.noreply.github.com>
2024-04-21 12:50:46 -0400

    Front-end app

---
+++ frontend/client/src/App.js
@@ -0,0 +1,63 @@
+
+import { Routes, Route, Navigate  } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import LogIn from "./pages/Login";
+import Homepage from './pages/Home';
+import PdfComp from "./pages/Document";
+import Navbar from "./pages/Navbar";
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+function App() {
+
+  const [userData, setUserData] = useState(null);
+
+  const navigate = useNavigate();
+
+  const getUser = async () => {
+    try {
+        const response = await axios.get("http://localhost:4000/login/sucess", { withCredentials: true });
+        console.log("response",response)
+        if (response.data.user) {
+          setUserData(response.data.user);
+        } else {
+          setUserData(null);
+        }
+    } catch (error) {
+      console.log("error", error);
+    }
+}
+
+
+useEffect(() => {
+  getUser()
+}, [])
+
+
+  return (
+      <>
+        <Routes>
+        <Route path='/' element={userData ? <Navigate to='/home' /> : <Navigate to='/login' />} />
+        <Route path='/login' element={<LogIn />} />
+        <Route path='/home' element={<Homepage />} />
+
+          {/* <Route path='/*' element={<Error />} /> */}
+          {/* <Route path='/' element={<Homepage userAcc={user} userSO={setUser} id="signInDiv"  />} />
+          <Route path='/' element={<Homepage userAcc={user} userSO={setUser} id="signInDiv"  />} /> */}  
+          {/* <Route path='/login' element={user ? <Navigate to='/' /> : <OpenInterface handleCallback={handleCallbackResponse} />} /> */}
+          {/* <Route path='/' element={<PdfComp />} /> */}
+        </Routes>
+      </>
+
+    // <>
+    //   {!display &&
+    //     <OpenInterface handleCallback={handleCallbackResponse} />
+    //   }
+    //   {user && display &&
+    //     <Work userAcc={user} display={setDisplay} userSO={setUser} id="signInDiv" />
+    //   }
+    // </>
+  );
+}
+
+export default App;
